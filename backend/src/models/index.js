2a7145a50afa04e1be12125979be6698f50d6956
Kunnan Hong <hongkunnan_1997@hotmail.com>
2022-04-03 01:02:29 +0200

    wip

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -4,14 +4,12 @@ const config = require('../config/config');
 const sequelize = new Sequelize(config);
 
 const SupplyCarbonMetadata = require('./supply_carbon_metadata')(sequelize, Sequelize.DataTypes);
-const Ghg = require('./ghg')(sequelize, Sequelize.DataTypes);
 const Transaction = require('./transaction')(sequelize, Sequelize.DataTypes);
 const Product = require('./product')(sequelize, Sequelize.DataTypes);
 const Merchant = require('./merchant')(sequelize, Sequelize.DataTypes);
 const SupplyChainParty = require('./supply_chain_party')(sequelize, Sequelize.DataTypes);
 const db = {
 	SupplyCarbonMetadata,
-	Ghg,
 	Transaction,
 	Product,
 	Merchant,
