81b4f66d7847274a267a0d28b71181b9cb0a2891
Kunnan Hong <hongkunnan_1997@hotmail.com>
2022-04-03 00:59:26 +0200

    wip

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -8,7 +8,7 @@ const config = {
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
-	host: 'localhost',
+	host: process.env.DB_HOST,
 	dialect: 'postgres',
 	port: 5432,
 };
