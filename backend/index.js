4b98f6787657bc6d06bb14cbd9e28ee072b5d849
Kunnan Hong <hongkunnan_1997@hotmail.com>
2022-04-03 03:22:51 +0200

    body-parser added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { sequelize, Transaction, Merchant, Product } = require('./src/models');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
