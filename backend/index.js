cf27760a80af951e2eef18742284ce604e10cbc7
Kunnan Hong <hongkunnan_1997@hotmail.com>
2022-04-03 09:44:46 +0200

    wip

--- backend/index.js
+++ backend/index.js
@@ -278,7 +278,7 @@ app.get('/emission_per_product', async (req, res) => {
 		const newProducts = products
 			.map((product) => {
 				const productObject = product;
-
+				console.log(product);
 				const emissions = product.product_batches.reduce((prev, curr) => prev + curr.quantity, 0);
 
 				productObject.total_emission = emissions;
