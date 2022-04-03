1214bd705c16ca7f15d4b287a8dd0e940493836b
Kunnan Hong <hongkunnan_1997@hotmail.com>
2022-04-03 08:52:43 +0200

    wip

--- backend/src/migrations/20220403014105-delete merchantId.js
+++ backend/src/migrations/20220403014105-delete merchantId.js
@@ -8,7 +8,7 @@ module.exports = {
 		 * Example:
 		 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
 		 */
-		await queryInterface.removeColumn('products', 'merchantId');
+		//await queryInterface.removeColumn('products', 'merchantId');
 	},
 
 	async down(queryInterface, Sequelize) {
