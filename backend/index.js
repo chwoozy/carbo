f6a92dd7cf95afc87233db3078bf1544e21b7164
Kunnan Hong <hongkunnan_1997@hotmail.com>
2022-04-03 03:08:11 +0200

    added create_merchant route

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const { sequelize, Transaction, Merchant } = require('./src/models');
+const { sequelize, Transaction, Merchant, Product } = require('./src/models');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+	// const merchant_data = req.body
+
+	// const merchant = await Merchant.create(merchant_data)
+	// const product = await Product.create({name: 'myproiduct', merchant_id: merchant.id})
 });
+
+app.post('/create_merchant', (req, res) => {
+  const merchant_data = req.body;
+  const merchant = await Merchant.create(merchant_data);
+  res.json(merchant)
+})
 
 app.listen(port, async () => {
 	console.log(`App listening on port ${port}`);
